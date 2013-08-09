3b3c986cb452a51c819059fca3b943d0b456b973
stankud <kudrow.stan@gmail.com>
2013-08-09 11:18:45 -0400

    Create morse_code.rb

---
+++ morse_code.rb
@@ -0,0 +1,40 @@
+MORSE_CODE = {
+  "a" => ".-",
+  "b" => "-...",
+  "c" => "-.-.",
+  "d" => "-..",
+  "e" => ".",
+  "f" => "..-.",
+  "g" => "--.",
+  "h" => "....",
+  "i" => "..",
+  "j" => ".---",
+  "k" => "-.-",
+  "l" => ".-..",
+  "m" => "--",
+  "n" => "-.",
+  "o" => "---",
+  "p" => ".--.",
+  "q" => "--.-",
+  "r" => ".-.",
+  "s" => "...",
+  "t" => "-",
+  "u" => "..-",
+  "v" => "...-",
+  "w" => ".--",
+  "x" => "-..-",
+  "y" => "-.--",
+  "z" => "--.."
+}
+
+def morse_encode_word(word)
+
+  morse_word = word.split("").collect { |let| MORSE_CODE[let]}
+	morse_word.join(" ")
+end
+
+def morse_encode(str)
+
+	morse_words = str.split(" ").collect{ |word| morse_encode_word(word)}
+	morse_words.join("  ")
+end
